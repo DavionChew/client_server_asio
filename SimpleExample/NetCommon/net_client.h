13d16ce332a662bbe8f9676d7e6be8266aee0ead
DavionChew <38470007+DavionChew@users.noreply.github.com>
2021-05-17 22:16:17 +0800

    update net_client.h to resolve endpoints and connection in Connect func.

--- SimpleExample/NetCommon/net_client.h
+++ SimpleExample/NetCommon/net_client.h
@@ -22,13 +22,16 @@ namespace olc{
 			// Connect to server with hostname/ip-addr and port
 			bool Connect(const std::string& host, const uint16_t port) {
 				try {
-					// Create connection
-					m_connection = std::make_unique<connection<T>>(); // TODO
-
 					// Resolve hostname/ip-addr into tangiable physical addr
 					asio::ip::tcp::resolver resolver(m_context);
 					asio::ip::tcp::resolver::results_type endpoints = resolver.resolve(host, std::to_string(port));
 
+					// Create connection
+					m_connection = std::make_unique<connection<T>>(
+						connection<T>::owner::client,
+						m_context,
+						asio::ip::tcp::socket(m_context), m_qMessagesIn);
+					
 					// Tell the connection object to connect to server
 					m_connection->ConnectToServer(endpoints);
 
